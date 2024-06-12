2966c546d1f7c7094de715913d81dfe7c9b9c146
Charlie <67763783+charlie5001@users.noreply.github.com>
2024-06-13 10:54:33 +1200

    Task 4 Complete

--- 01_Help_GUI_V3.py
+++ 01_Help_GUI_V3.py
@@ -11,14 +11,24 @@ class Converter:
         self.converter_frame = Frame(width=600,  height=600, bg=background_color)
         self.converter_frame.grid()
         #Temperature Conversion Heading (row 0)
-        self.temp_converter_label = Label(text="Temperature Converter",
-                                        font=("Arial", "16", "bold"),bg=background_color,
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                        font=("Arial", "16", "bold"),
+                                        bg=background_color,
                                         padx=10, pady=10)
         self.temp_converter_label.grid(row=0)
         
         # Help Button (row 1)
-        self.help_button = Button(self.converter_frame, text="help", padx=10, pady=10)
+        self.help_button = Button(self.converter_frame,
+                                  text="Help", font=("Arial", "14"),
+                                  padx=10, pady=10, 
+                                  command=self.help)
+    
         self.help_button.grid(row=1)
+
+    def help(self):
+        print("You asked for help")
+        get_help = help(self)
+        get_help.help_text.configure(text="Help text goes here")
 
 # main routine
 if __name__ == "__main__":
