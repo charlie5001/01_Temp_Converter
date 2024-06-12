0de2e7c663f4db0594079dfb74978958d2222958
Charlie <67763783+charlie5001@users.noreply.github.com>
2024-06-12 12:56:43 +1200

    Create 01_Help_GUI.py

---
+++ 01_Help_GUI.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import random
+
+class Converter:
+    def __init__(self, parent):
+        print("hello world")
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter(root)
+    root.mainloop()
