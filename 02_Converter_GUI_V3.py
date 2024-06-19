88e82eed720201abd16c6085a983162f084f4425
Charlie <67763783+charlie5001@users.noreply.github.com>
2024-06-19 12:04:31 +1200

    Vid 10 complete

---
+++ 02_Converter_GUI_V3.py
@@ -0,0 +1,92 @@
+from tkinter import *
+from functools import partial
+import random
+
+class Converter:
+    def __init__(self, parent):
+        # Formatting variables...
+        background_color = "light blue"
+
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(width=300,  height=1200, bg=background_color)
+        self.converter_frame.grid()
+        #Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                        font=("Arial 16 bold"),
+                                        bg=background_color,
+                                        padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+        
+        #User Instructions row(1)
+        self.temp_instructions_label = Label(self.converter_frame, text="Type in the amount to be "
+                                             "converted and then push "
+                                             "one of the buttons below...",
+                                             font="Arial 10 italic", wrap=250, justify=LEFT, bg = background_color, padx=10, pady=10)
+        self.temp_instructions_label.grid(row=1)
+
+        #Temperature entry box (row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20, font="Arial 14 bold")
+        self.to_convert_entry.grid(row=2)
+
+        #conversion buttons frame (row 3)
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
+
+        self.to_c_button = Button(self.conversion_buttons_frame, text="To Centigrade", font="Arial 10 bold", bg="yellow", padx=10, pady=10, command=lambda: self.temp_convert(-459))
+        self.to_c_button.grid(row=0, column=0)
+
+        self.to_f_button = Button(self.conversion_buttons_frame, text="To Farhenheit", font="Arial 10 bold", bg="pink", padx=10, pady=10, command=lambda: self.temp_convert(-273))
+        self.to_f_button.grid(row=0, column=1)
+        #Answer Label (row 4)
+        self.converted_label = Label(self.converter_frame, font="Arial 12 bold",
+                                     fg="purple", bg=background_color,
+                                     pady=10, text="Conversion goes here")
+        self.converted_label.grid(row=4)
+
+        #History / Help button frame (row 5)
+        self.hist_help_frame = Frame(self.converter_frame)
+        self.hist_help_frame.grid(row=5, pady=10)
+
+        self.calc_hist__button = Button(self.hist_help_frame, font="Arial 12 bold",
+                                        text="Calculation History", width=15)
+        self.calc_hist__button.grid(row=0, column=0)
+
+        self.help_button = Button(self.hist_help_frame, font="Arial 12 bold",
+                                  text="Help", width=5)
+        self.help_button.grid(row=0, column=1)
+
+
+    def temp_convert(self, to):
+        print(to)
+
+        #Retrieve amount entered into Entry Fiel d
+        to_convert = self.to_convert_entry.get()
+        #Check amount is a valid number
+        try:
+            to_convert = float(to_convert)
+            print("yay")
+        except ValueError:
+            print("oops")
+        #Convert to F
+
+        #Convert to C
+
+        # Round!!
+
+        #Display answer
+
+        #Add answer to list for History
+
+def to_deg(self):
+    which = 1
+    self.change(which)
+
+def to_far(self):
+    which = 2
+    self.change(which)
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter(root)
+    root.mainloop()
