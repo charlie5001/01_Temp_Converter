314e5efae2ff94db57d5ce1a4ad211a7bdedfc97
Charlie <67763783+charlie5001@users.noreply.github.com>
2024-06-25 14:44:42 +1200

    Task 14 done

---
+++ 08_Data_to_txtV2.py
@@ -0,0 +1,20 @@
+# Data to be outputted
+
+data = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
+
+#Get filename, can't be blank / invalid
+#assume valid data for now
+filename = input("Enter a Filename (leave off the extension): ")
+
+#add .txt suffix
+filename= filename + ".txt"
+
+#create file to hold data
+f = open(filename, 'w+')
+
+#add new line at the end of each item
+for item in data:
+    f.write(item + "\n")
+
+#close file
+f.close()
